df244145cd3b2c6f648641e436d8fe320cacf0d4
rufus <rufat.khaslarov@gmail.com>
2016-12-29 10:07:09 +0400

    use

--- package.js
+++ package.js
@@ -8,6 +8,6 @@ Package.describe({
 
 Package.onUse(function(api) {
 	api.versionsFrom('1.2.1');
-	api.use(['ecmascript', 'rkhaslarov:md5']);
+	api.use(['ecmascript', 'rkhaslarov:md5@1.0.0']);
 	api.addFiles(['string.js']);
 });
